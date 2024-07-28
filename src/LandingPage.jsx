540c92a15d5d83a6e2a2da7a440e30a0f9c5020d
JLopezRuiz <jlopezru@wellesley.edu>
2024-07-27 20:04:41 -0700

    first commit

---
+++ src/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            Hi, I'm your Budget Buddy!
+        </div>
+    );
+};
+
+export default LandingPage;
